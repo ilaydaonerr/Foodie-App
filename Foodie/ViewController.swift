b12bc0f10b8e3d71a7394f8f4f7d71887821ac13
ilaydaoner <ilaydacem2405@gmail.com>
2022-12-01 13:40:58 +0300

    Initial Commit

---
+++ Foodie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodie
+//
+//  Created by İlayda Öner on 1.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
